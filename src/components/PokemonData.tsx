e3dac503027bdcbec372f7f2940e5fed83b86377
Angela <angelakgo20@gmail.com>
2024-04-24 18:56:28 -0300

    delete unused files

--- src/components/PokemonData.tsx
+++ src/components/PokemonData.tsx
@@ -44,168 +44,172 @@ export default function PokemonData({ pokemon, isCatched }: IPokemonData) {
       h="100%"
       overflowY="scroll"
       padding={{ base: "12px", sm: "20px", md: "40px" }}>
-      <Flex
-        w="100%"
-        h="100%"
-        flexDirection={"column"}
-        alignItems="center"
-        justifyContent="center"
-        paddingTop={{ base: "20px" }}>
-        {resSuccess === "success" && (
-          <Alert status="success">
-            <AlertIcon />
-            <AlertTitle>Your pokemon was catched!</AlertTitle>
-          </Alert>
-        )}
-        {resSuccess.includes("error") && (
-          <Alert status="error">
-            <AlertIcon />
-            <AlertTitle>
-              Sorry, something went wrong trying to catch the pokemon.
-            </AlertTitle>
-            <AlertDescription>
-              Please, refresh the page and try again
-            </AlertDescription>
-          </Alert>
-        )}
-        <Text
-          fontSize="3xl"
-          pb="10"
-          textTransform="capitalize"
-          color="gray.100">
-          {pokemon.name}
-        </Text>
-        <Stack
-          direction={["column-reverse", "row"]}
-          spacing="8"
-          alignItems="center">
-          <Image
-            width={100}
-            height={100}
-            style={{ width: "180px", height: "auto" }}
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemon.id}.gif`}
-            alt={`${pokemon.name}'s image`}
-          />
-          <Box>
-            {isCatched ? (
-              <Text
-                px="5"
-                py="2"
-                border="1px"
-                borderRadius="8px"
-                borderColor="orange.100"
-                fontSize="md"
-                fontWeight={700}
-                color="gray.100">
-                Is catched!
-              </Text>
-            ) : (
-              <Button name="chatch" onClick={handleChange}>
-                Catch
-              </Button>
-            )}
-          </Box>
-        </Stack>
-        <Stack
-          w="full"
-          spacing="5"
-          pt="6"
-          maxWidth={{ base: "100%", lg: "50%" }}>
-          <Stack spacing="5" p="5" maxWidth={{ base: "max-content" }}>
-            <Stack direction={["column", "row"]} spacing="5">
-              <Stack direction="column">
+      {pokemon.name ? (
+        <Flex
+          w="100%"
+          h="100%"
+          flexDirection={"column"}
+          alignItems="center"
+          justifyContent="center"
+          paddingTop={{ base: "20px" }}>
+          {resSuccess === "success" && (
+            <Alert status="success">
+              <AlertIcon />
+              <AlertTitle>Your pokemon was catched!</AlertTitle>
+            </Alert>
+          )}
+          {resSuccess.includes("error") && (
+            <Alert status="error">
+              <AlertIcon />
+              <AlertTitle>
+                Sorry, something went wrong trying to catch the pokemon.
+              </AlertTitle>
+              <AlertDescription>
+                Please, refresh the page and try again
+              </AlertDescription>
+            </Alert>
+          )}
+          <Text
+            fontSize="3xl"
+            pb="10"
+            textTransform="capitalize"
+            color="gray.100">
+            {pokemon.name}
+          </Text>
+          <Stack
+            direction={["column-reverse", "row"]}
+            spacing="8"
+            alignItems="center">
+            <Image
+              width={100}
+              height={100}
+              style={{ width: "180px", height: "auto" }}
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemon.id}.gif`}
+              alt={`${pokemon.name}'s image`}
+            />
+            <Box>
+              {isCatched ? (
                 <Text
-                  px="2"
-                  borderBottom="1px"
-                  borderBottomColor="orange.100"
+                  px="5"
+                  py="2"
+                  border="1px"
+                  borderRadius="8px"
+                  borderColor="orange.100"
                   fontSize="md"
                   fontWeight={700}
                   color="gray.100">
-                  Peso
+                  Is catched!
                 </Text>
-                <Text color="gray.100">{pokemon.weight} lb</Text>
-              </Stack>
-              <Stack direction="column">
-                <Text
-                  px="2"
-                  borderBottom="1px"
-                  borderBottomColor="orange.100"
-                  fontSize="md"
-                  fontWeight={700}
-                  color="gray.100">
-                  Altura
-                </Text>
-                <Text color="gray.100">{pokemon.height} ft</Text>
-              </Stack>
-              <Stack direction="column">
-                <Text
-                  px="2"
-                  borderBottom="1px"
-                  borderBottomColor="orange.100"
-                  fontSize="md"
-                  fontWeight={700}
-                  color="gray.100">
-                  Movimientos
-                </Text>
-                <Text color="gray.100">{pokemon.moves.length}</Text>
-              </Stack>
-              <Stack direction="column">
-                <Text
-                  px="2"
-                  borderBottom="1px"
-                  borderBottomColor="orange.100"
-                  fontSize="md"
-                  fontWeight={700}
-                  color="gray.100">
-                  Experiencia
-                </Text>
-                <Text color="gray.100">{pokemon.base_experience}</Text>
-              </Stack>
-            </Stack>
-            <Stack>
-              <Text
-                px="2"
-                borderBottom="1px"
-                borderBottomColor="orange.100"
-                fontSize="md"
-                fontWeight={700}
-                color="gray.100">
-                Tipos
-              </Text>
-              <HStack flexDirection="row" wrap="wrap" spacing="2">
-                {pokemon.types.map((item: any, index: number) => (
-                  <Text
-                    key={`${item.type.name}${index}`}
-                    textTransform="capitalize"
-                    color="gray.100">
-                    {item.type.name}
-                  </Text>
-                ))}
-              </HStack>
-            </Stack>
+              ) : (
+                <Button name="chatch" onClick={handleChange}>
+                  Catch
+                </Button>
+              )}
+            </Box>
           </Stack>
-
-          {pokemon.stats.map((item: any, index: number) => (
-            <Stack
-              key={index}
-              spacing="5"
-              p="5"
-              bg="gray.100"
-              borderRadius="xl">
-              <Stack w="100%">
-                <Text fontSize="xs" textTransform="capitalize">
-                  {item.stat.name}
+          <Stack
+            w="full"
+            spacing="5"
+            pt="6"
+            maxWidth={{ base: "100%", lg: "50%" }}>
+            <Stack spacing="5" p="5" maxWidth={{ base: "max-content" }}>
+              <Stack direction={["column", "row"]} spacing="5">
+                <Stack direction="column">
+                  <Text
+                    px="2"
+                    borderBottom="1px"
+                    borderBottomColor="orange.100"
+                    fontSize="md"
+                    fontWeight={700}
+                    color="gray.100">
+                    Peso
+                  </Text>
+                  <Text color="gray.100">{pokemon.weight} lb</Text>
+                </Stack>
+                <Stack direction="column">
+                  <Text
+                    px="2"
+                    borderBottom="1px"
+                    borderBottomColor="orange.100"
+                    fontSize="md"
+                    fontWeight={700}
+                    color="gray.100">
+                    Altura
+                  </Text>
+                  <Text color="gray.100">{pokemon.height} ft</Text>
+                </Stack>
+                <Stack direction="column">
+                  <Text
+                    px="2"
+                    borderBottom="1px"
+                    borderBottomColor="orange.100"
+                    fontSize="md"
+                    fontWeight={700}
+                    color="gray.100">
+                    Movimientos
+                  </Text>
+                  <Text color="gray.100">{pokemon.moves.length}</Text>
+                </Stack>
+                <Stack direction="column">
+                  <Text
+                    px="2"
+                    borderBottom="1px"
+                    borderBottomColor="orange.100"
+                    fontSize="md"
+                    fontWeight={700}
+                    color="gray.100">
+                    Experiencia
+                  </Text>
+                  <Text color="gray.100">{pokemon.base_experience}</Text>
+                </Stack>
+              </Stack>
+              <Stack>
+                <Text
+                  px="2"
+                  borderBottom="1px"
+                  borderBottomColor="orange.100"
+                  fontSize="md"
+                  fontWeight={700}
+                  color="gray.100">
+                  Tipos
                 </Text>
-                <Progress
-                  bg="gray.300"
-                  borderRadius="full"
-                  value={item.base_stat}
-                />
+                <HStack flexDirection="row" wrap="wrap" spacing="2">
+                  {pokemon.types.map((item: any, index: number) => (
+                    <Text
+                      key={`${item.type.name}${index}`}
+                      textTransform="capitalize"
+                      color="gray.100">
+                      {item.type.name}
+                    </Text>
+                  ))}
+                </HStack>
               </Stack>
             </Stack>
-          ))}
-        </Stack>
-      </Flex>
+
+            {pokemon.stats.map((item: any, index: number) => (
+              <Stack
+                key={index}
+                spacing="5"
+                p="5"
+                bg="gray.100"
+                borderRadius="xl">
+                <Stack w="100%">
+                  <Text fontSize="xs" textTransform="capitalize">
+                    {item.stat.name}
+                  </Text>
+                  <Progress
+                    bg="gray.300"
+                    borderRadius="full"
+                    value={item.base_stat}
+                  />
+                </Stack>
+              </Stack>
+            ))}
+          </Stack>
+        </Flex>
+      ) : (
+        <div>Sorry, We cannot show the pokemon right now. Try later</div>
+      )}
     </Center>
   );
 }
